8468ccb96739c6af8d6d6f5ae593da48c6edad98
Kelvin <chongkelv@gmail.com>
2025-04-17 21:46:22 +0800

    refactor: remove unused routes from App component and restore style imports in main file

--- prism-project/frontend/src/App.tsx
+++ prism-project/frontend/src/App.tsx
@@ -1,19 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
